bd250f72c3cfa490233b8e743e64816545af3575
Rebekah Jones <jones.rebekah17@gmail.com>
2019-08-10 09:37:17 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,14 +14,37 @@ with open(bank_csv,'r') as csvfile:
 #to print first row
     ##for row in csvreader:
         ##print(row[0])
-    #to find the first two, set both variables to zero
+    #set variables to zero and create empty lists
     months_total = 0 
     total_profit = 0
+    first_row = next(csvreader)
+    previous_change = float(first_row[1])
+    net_change_list = []
+    month_list = []
+    greatest_inc = []
+    greatest_dec = []
     #loop through the data
     for row in csvreader:
-        #sum of second row, needs to be cast to a float
-        total_profit = total_profit + float(row[1])   
         #count of first row
         months_total = months_total + 1
+        #sum of second row, needs to be cast to a float
+        total_profit = total_profit + float(row[1])   
+        #total_avg = total_profit / months_total
+        #finding avg. change of profit
+        net_change = float(row[1]) - previous_change
+        previous_change = float(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_list = month_list + [row[0]]
+        
+        #finding greatest increase and decrease
+        greatest_inc = max(net_change_list)
+        greatest_dec = min(net_change_list)
+       
+        
+    avg_change = sum(net_change_list) / len(net_change_list)
+    
     print(months_total)
     print(total_profit)
+    print(round(avg_change,2))
+    print(int(greatest_inc))
+    print(int(greatest_dec))
